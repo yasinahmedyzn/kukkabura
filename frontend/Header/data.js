57f946b7bc5d62b25cd6208d8567b3a83f571529
yasinahmedyzn <mdyasinahmed@gmail.com>
2025-07-21 00:58:44 +0600

    laptop push

---
+++ frontend/Header/data.js
@@ -0,0 +1,22 @@
+export const links = [
+  {
+    id: 1,
+    url: "/",
+    text: "home",
+  },
+  {
+    id: 2,
+    url: "/about",
+    text: "form-data",
+  },
+  {
+    id: 3,
+    url: "/services",
+    text: "fetch-data",
+  },
+  {
+    id: 4,
+    url: "/contact",
+    text: "demo",
+  },
+];
