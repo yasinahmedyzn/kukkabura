b4e7dcedd40a3080fee37ee13cfca55ccce28bdb
yasinahmedyzn <mdyasinahmed@gmail.com>
2025-07-24 14:23:20 +0600

    office push 24/07/2023

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,11 +1,13 @@
 import "./App.css";
 import Navbar from "../Header/Navbar";
 import HomePage from "../Pages/Home";
+import Example from "../Product/Product";
 function App() {
   return (
     <>
       <Navbar />
       <HomePage />
+      <Example />
     </>
   );
 }
