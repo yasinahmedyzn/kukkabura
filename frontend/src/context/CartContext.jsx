bff5dbdd6f6dce8bccda61b2aaf14ce17cc85b45
yasinahmedyzn <mdyasinahmed@gmail.com>
2025-08-14 18:19:59 +0600

    Api corrected

--- frontend/src/context/CartContext.jsx
+++ frontend/src/context/CartContext.jsx
@@ -1,5 +1,5 @@
 import React, { createContext, useContext, useState } from "react";
-import api from "../api/api"; // Your axios instance
+import api from "../api/Api"; // Your axios instance
 
 const CartContext = createContext();
 
