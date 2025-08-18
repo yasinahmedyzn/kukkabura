cc21b102bfd7484e11989645f671a6cbceb669cb
yasinahmedyzn <mdyasinahmed@gmail.com>
2025-08-18 15:27:58 +0600

    add to cart working for web

--- frontend/src/api/Api.jsx
+++ frontend/src/api/Api.jsx
@@ -2,7 +2,7 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: import.meta.env.VITE_API_BASE_URL || "http://localhost:5000",
+  baseURL: import.meta.env.VITE_API_URL
 });
 
 // Attach token to every request if exists
