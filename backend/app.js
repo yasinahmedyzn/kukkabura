88492173c97d3e5fbaa97944fae84229587a15a9
yasinahmedyzn <mdyasinahmed@gmail.com>
2025-08-19 14:59:10 +0600

    no trailing slash on cors policy

--- backend/app.js
+++ backend/app.js
@@ -10,9 +10,13 @@ const app = express();
 // Middleware
 app.use(express.json());
 app.use(cors({
-  origin: ['https://kukkabura-git-main-mdyasinahmed-7053s-projects.vercel.app/', 'http://localhost:5173'],
+  origin: [
+    'https://kukkabura-git-main-mdyasinahmed-7053s-projects.vercel.app',
+    'http://localhost:5173'
+  ],
   methods: ['GET', 'POST', 'PUT', 'DELETE'],
 }));
+
 
 // Connect to DB
 connectDB();
